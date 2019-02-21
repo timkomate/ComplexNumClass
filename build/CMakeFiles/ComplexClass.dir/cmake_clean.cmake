518783423d8a34b7fa155de171b766bb5618b9f5
Máté Timkó <timko.mate@gmail.com>
2019-02-21 18:50:44 +0100

    organizing

---
+++ build/CMakeFiles/ComplexClass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ComplexClass.dir/main.cpp.o"
+  "CMakeFiles/ComplexClass.dir/src/Complex.cpp.o"
+  "../bin/ComplexClass.pdb"
+  "../bin/ComplexClass"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ComplexClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
